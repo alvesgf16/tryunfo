f5fa22f2a5c81e8cdd93e2f4bb86a9ed4c4a4da7
alvesgf16 <alvesgabriel1994@gmail.com>
2021-10-13 14:48:57 -0300

    Cria componente generico para inputs de texto

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Input from './Input';
+
+export default class Form extends React.Component {
+  render() {
+    return (
+      <Input type="text" dataTestId="name-input" id=''/>
+    );
+  }
+}
---
+++ src/components/Input.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Input extends React.Component {
+  render() {
+    const { type, dataTestId } = this.props;
+    return (
+      <div>
+        <label htmlFor={ dataTestId }>
+          Name:
+          <input type={ type } data-testid={ dataTestId } id={ dataTestId }/>
+        </label>
+      </div>
+    );
+  }
+}
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  dataTestId: PropTypes.string.isRequired,
+}
