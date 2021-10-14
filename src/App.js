c8702703eee4e5efc8b9d6a787986464dbd36362
alvesgf16 <alvesgabriel1994@gmail.com>
2021-10-14 09:26:56 -0300

    Exibe lista de cartas criadas

--- src/App.js
+++ src/App.js
@@ -101,6 +101,7 @@ class App extends React.Component {
       cardTrunfo,
       hasTrunfo,
       isSaveButtonDisabled,
+      cards,
     } = this.state;
 
     return (
@@ -130,6 +131,17 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+        {cards.map((card, index) => (<Card
+          key={ `card${index}` }
+          cardName={ card.cardName }
+          cardDescription={ card.cardDescription }
+          cardAttr1={ card.cardAttr1 }
+          cardAttr2={ card.cardAttr2 }
+          cardAttr3={ card.cardAttr3 }
+          cardImage={ card.cardImage }
+          cardRare={ card.cardRare }
+          cardTrunfo={ card.cardTrunfo }
+        />))}
       </div>
     );
   }
