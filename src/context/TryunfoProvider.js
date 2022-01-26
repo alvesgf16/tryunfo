c9c7aef2945045d600ba0bcd0cff2234eb27964e
alvesgf16 <alvesgabriel1994@gmail.com>
2022-01-26 14:40:02 -0300

    refactor: move App functions to context

--- src/context/TryunfoProvider.js
+++ src/context/TryunfoProvider.js
@@ -20,7 +20,94 @@ function TryunfoProvider({ children }) {
     cards: [],
   });
 
-  const contextValue = { state, setState };
+  function enableSaveButton() {
+    setState(({
+      cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    }) => {
+      const cardAttrsStrs = [cardAttr1, cardAttr2, cardAttr3];
+      const cardAttrs = cardAttrsStrs.map((cardAttr) => parseInt(cardAttr, 10));
+      const attrMax = 90;
+      const totalMax = 210;
+      return ({ isSaveButtonDisabled: !(cardName && cardDescription && cardImage
+      && cardRare && cardAttrs.reduce((acc, cur) => acc + cur) <= totalMax
+      && cardAttrs.every((cardAttr) => cardAttr >= 0 && cardAttr <= attrMax)) });
+    });
+  }
+
+  function onInputChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    setState({ [name]: value });
+    enableSaveButton();
+  }
+
+  function deleteCard({ target }) {
+    const selectedCardName = target.parentElement.id;
+    const { cards } = state;
+    const selectedCard = cards
+      .find(({ cardName }) => cardName === selectedCardName);
+    const isTrunfo = selectedCard.cardTrunfo;
+
+    setState((prevState) => (isTrunfo ? ({
+      cards: prevState.cards.filter(({ cardName }) => cardName !== selectedCardName),
+      cardTrunfo: !isTrunfo,
+      hasTrunfo: !isTrunfo,
+    }) : ({
+      cards: prevState.cards.filter(({ cardName }) => cardName !== selectedCardName),
+    })
+    ));
+  }
+
+  function onSaveButtonClick(event) {
+    event.preventDefault();
+
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+    } = state;
+
+    const cardInfo = {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      deleteCard,
+    };
+
+    setState((prevState) => ({
+      cardName: '',
+      cardDescription: '',
+      cardImage: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardRare: 'normal',
+      hasTrunfo: cardTrunfo,
+      isSaveButtonDisabled: true,
+      cards: [...prevState.cards, cardInfo],
+    }));
+  }
+
+  const contextValue = { state, onInputChange, onSaveButtonClick };
 
   return (
     <TryunfoContext.Provider value={ contextValue }>
