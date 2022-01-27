acae6cb6775802e71f45076138b00c5833739a08
alvesgf16 <alvesgabriel1994@gmail.com>
2022-01-27 11:50:53 -0300

    refactor: move deck rendering to an specific component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
-import TryunfoContext from './context/TryunfoContext';
 import Filters from './components/Filters';
+import Deck from './components/Deck';
 
 function App() {
-  const { state } = useContext(TryunfoContext);
-
-  const {
-    nameFilter,
-    rareFilter,
-    trunfoFilter,
-    cards,
-  } = state;
-
   return (
     <div>
       <h1>Tryunfo</h1>
       <Form />
       <Card info={ state } />
       <Filters />
-      { cards.filter((card) => {
-        if (trunfoFilter) return card.cardTrunfo && !card.hasTrunfo;
-        const getRareFilter = () => (rareFilter === 'todas'
-          ? true : card.cardRare === rareFilter);
-        const result = card.cardName.includes(nameFilter) && getRareFilter();
-        return result;
-      })
-        .map((card, i) => (<Card key={ `card${i + 1}` } deckCard info={ card } />)) }
+      <Deck />
     </div>
   );
 }
---
+++ src/components/Deck.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import TryunfoContext from '../context/TryunfoContext';
+import Card from './Card';
+
+export default function Deck() {
+  const { state } = useContext(TryunfoContext);
+
+  const {
+    nameFilter,
+    rareFilter,
+    trunfoFilter,
+    cards,
+  } = state;
+
+  return (
+    <div>
+      { cards.filter((card) => {
+        const { cardTrunfo, hasTrunfo, cardRare, cardName } = card;
+
+        if (trunfoFilter) return cardTrunfo && !hasTrunfo;
+
+        const getRareFilter = () => (
+          rareFilter === 'todas' ? true : cardRare === rareFilter
+        );
+        const result = cardName.includes(nameFilter) && getRareFilter();
+
+        return result;
+      })
+        .map((card, i) => (<Card key={ `card${i + 1}` } deckCard info={ card } />)) }
+    </div>
+  );
+}
