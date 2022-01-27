a941614969c73912948219ea306f1d2fbcbddcc0
alvesgf16 <alvesgabriel1994@gmail.com>
2022-01-27 12:35:59 -0300

    refactor: turn TextArea into function

--- src/components/inner-components/TextArea.js
+++ src/components/inner-components/TextArea.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class TextArea extends React.Component {
-  render() {
-    const { value, name, dataTestId, label, onChange } = this.props;
-    return (
-      <div>
-        <label htmlFor={ dataTestId }>
-          { label }
-          <textarea
-            value={ value }
-            data-testid={ dataTestId }
-            name={ name }
-            id={ dataTestId }
-            onChange={ onChange }
-          />
-        </label>
-      </div>
-    );
-  }
+export default function TextArea(props) {
+  const { value, name, dataTestId, label, onChange } = props;
+  return (
+    <label htmlFor={ dataTestId }>
+      { label }
+      <textarea
+        value={ value }
+        data-testid={ dataTestId }
+        name={ name }
+        id={ dataTestId }
+        onChange={ onChange }
+      />
+    </label>
+  );
 }
 
 TextArea.propTypes = {
