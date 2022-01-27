b35cb96f3adee5b0861f1d5a38b570f410ed2113
alvesgf16 <alvesgabriel1994@gmail.com>
2022-01-27 12:08:46 -0300

    fix: add context back to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
 import Filters from './components/Filters';
 import Deck from './components/Deck';
+import TryunfoContext from './context/TryunfoContext';
 
 function App() {
+  const { state } = useContext(TryunfoContext);
   return (
     <div>
       <h1>Tryunfo</h1>
